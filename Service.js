b176c49687d397d611a0f549e4685a4c4ab86d2f
capn <jamesgreen.311@gmail.com>
2021-06-10 13:47:53 -0400

    Add Meeting Type
    - modified confirmation email
    - removed testing code

--- Service.js
+++ Service.js
@@ -18,26 +18,27 @@ function sendNotification(data) {
     const inPersonContent = `This month's host is ${data.hostName}. The host's address is ${data.hostLocation}.`
     const zoomContent = `To Join Zoom Meeting<br>${data.zoomLink}<br><br>Meeting ID: ${data.zoomMeetingId}<br>Passcode: ${data.zoomPasscode}<br><br>`
     const contactInfo = data.contactEmail ? `For questions, please contact ${data.contactEmail} <br><br>` : "";
-    
+
     let body = `Registered Name: ${data.firstName} ${data.lastName} <br>Registered Email: ${data.email}<br><br>
-        Thank you for registering for this month's critique session <br><br>
+        Thank you for registering for this month's critique session. <br><br>
         ${contactInfo}
         If you need a reminder about event details, please revisit the <a href='${regForm}'>registration form</a> or the MRAA website 
-        <a href='${eventsPage}'>Events page</a>`
+        <a href='${eventsPage}'>Events page</a>.`
     if (meetingtype.toLowerCase() === "in person") {
-        body =+ `<br><br>Host<br><br>${inPersonContent}`;
+        body += `<br><br><strong>Host Information</strong><br>${inPersonContent}`;
     } else if (meetingtype.toLowerCase() === "virtual") {
-        body =+ `<br><br>Zoom Info <br><br>${zoomContent}`;
+        body += `<br><br>Zoom Information <br>${zoomContent}`;
     } else if (meetingtype.toLowerCase() === "hybrid") {
-        body =+ `<br><br>Please RSVP to the host if you plan to attend in person.`
-        body =+ `<br><br>Host<br><br>${inPersonContent}`
-        body =+ `<br><br>Zoom Info <br><br>${zoomContent}`;
+        body += `<br><br>Please RSVP to the host if you plan to attend in person.
+            <br><br><strong>Host Information</strong><br>${inPersonContent}
+            <br><br>Zoom Information <br>${zoomContent}`
     }
-
+    
     MailApp.sendEmail({
         to: data.sendTo,
         cc: `${getCoordinatorEmail()}, ${getHostEmail()}`,
         //cc: `${getHostEmail()}`, TESTING
+        to: "jamesgreen.311@gmail.com",
         replyTo: getHostEmail(),
         subject: "MRAA Critique Registration",
         htmlBody: body
